5d6e537a8db10c6fd6b2af38a3432a3fbced74ec
Alexi Peck <alexinicolaspeck@gmail.com>
2022-12-03 00:11:11 +1100

    3 ways of doing Day 1

---
+++ src/main.rs
@@ -0,0 +1,68 @@
+use std::collections::{BTreeMap, HashMap};
+use std::env;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::ops::Index;
+use std::path::Path;
+
+fn get_lines(path: &Path) -> Vec<String> {
+    match File::open(path) {
+        Ok(file) => BufReader::new(file)
+            .lines()
+            .map(|t| t.unwrap())
+            .collect::<Vec<String>>(),
+        Err(err) => {
+            panic!("Error opening file: {}", err);
+        }
+    }
+}
+
+#[allow(dead_code)]
+fn day_1(input_path: &Path) {
+    {
+        let mut elves: HashMap<usize, usize> = HashMap::new();
+        let mut elf_counter: usize = 0;
+        for line in get_lines(input_path).iter() {
+            if line.is_empty() {
+                elf_counter += 1;
+            } else {
+                *elves.entry(elf_counter).or_insert(0) += line.parse::<usize>().unwrap();
+            }
+        }
+        println!("{}", elves.values().max().unwrap());
+    }
+    
+    {
+        let mut elves: [usize; 236] = [0; 236];
+        let mut elf_counter: u16 = 0;
+        for line in get_lines(input_path).iter() {
+            if line.is_empty() {
+                elf_counter += 1;
+            } else {
+                elves[elf_counter as usize] += line.parse::<usize>().unwrap();
+            }
+        }
+        println!("{}", elves.iter().max().unwrap());
+    }
+
+    {
+        let mut elves: Vec<usize> = Vec::new();
+        let mut elf_counter: u16 = 0;
+        for line in get_lines(input_path).iter() {
+            if line.is_empty() {
+                elf_counter += 1;
+            } else {
+                if elves.get(elf_counter as usize).is_none() {
+                    elves.insert(elf_counter as usize, 0)
+                };
+                elves[elf_counter as usize] += line.parse::<usize>().unwrap();
+            }
+        }
+        println!("{}", elves.iter().max().unwrap());
+    }
+}
+
+fn main() {
+    let current_directory = env::current_dir().unwrap();
+    day_1(&current_directory.join("input_day_1.txt"));
+}
